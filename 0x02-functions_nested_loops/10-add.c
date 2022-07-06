c2b4a43aa487b0c9e69bbf23a9608f9b434d310c
Mihlali Faku <lihlefaku.lf@gmail.com>
2022-07-05 19:47:05 -0700

    ccode

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addition
+ * @a: integer 1 to be added
+ * @b: integer 2 to be added
+ *
+ * Return: check code
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
