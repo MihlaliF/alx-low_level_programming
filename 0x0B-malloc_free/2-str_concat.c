446f84d65c5c3489added69061ab015e3aa68ff3
Mihlali Faku <lihlefaku.lf@gmail.com>
2022-07-27 01:59:08 -0700

    gcc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,49 +3,40 @@
 #include "main.h"
 
 /**
- * str_concat - function that concatenates two strings
- *
- * @s1: string of chars
- * @s2: string of chars
- *
- * Return: address of the newly allocated memory
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: check code
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1, len2;
-	unsigned int i, j;
-	char *str_copy;
-	char *tmp1 = s1;
-	char *tmp2 = s2;
+	char *strsum;
+	unsigned int i, j, k, l, m;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 
-	i = 0;
-	while (*s1++)
-		i++;
-	len1 = i;
-	s1 = tmp1;
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
 
-	i = 0;
-	while (*s2++)
-		i++;
-	len2 = i;
-	s2 = tmp2;
+	k = i + j;
 
-	str_copy = malloc((len1 + len2) * sizeof(char) + 1);
-	if (str_copy == NULL)
+	strsum = malloc(sizeof(char) * (k + 1));
+
+	if (strsum == NULL)
 		return (NULL);
 
-	j = 0;
-	while (j < len1)
-	{
-		str_copy[j] = s1[j];
-		j++;
-	}
-	str_copy[j] = '\0';
-	return (str_copy);
+	for (l = 0; s1[l] != '\0'; l++)
+		strsum[l] = s1[l];
+
+	for (m = 0; s2[m] != '\0'; m++)
+		strsum[l++] = s2[m];
+
+	return (strsum);
+	free(strsum);
 }
