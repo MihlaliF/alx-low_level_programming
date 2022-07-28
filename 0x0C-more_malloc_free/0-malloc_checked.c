6174c17e3fafc0fff104f12c0cebbc8a52539f95
Mihlali Faku <lihlefaku.lf@gmail.com>
2022-07-28 12:24:17 -0700

    calloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * mallo_checked - allocates memory using malloc
+ * malloc_checked - allocates memory using malloc
  * @b: size of memory to be allocated
  * Return: check code
  */
